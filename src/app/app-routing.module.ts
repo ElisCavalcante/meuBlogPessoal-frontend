47d4d634071de5f91c795129128c0d23df40cf48
ElisCavalcante <elisabeth.rsc@outlook.com>
2022-09-04 11:25:55 -0300

    adicionando rotas cadastre-se e cancelar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuCadastrarComponent } from './menu-cadastrar/menu-cadastrar.component';
+import { MenuEntrarComponent } from './menu-entrar/menu-entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"" , redirectTo:" MenuEntrarComponent",pathMatch:"full"},
+
+    {path:"entrar",component:MenuEntrarComponent},
+    {path:"cadastrar",component:MenuCadastrarComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
