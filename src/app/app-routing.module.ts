66b87a628c3c06412b467633f0bf0290b1a44bdd
ElisCavalcante <elisabeth.rsc@outlook.com>
2022-09-07 15:46:36 -0300

    consumo de api - login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 import { MenuCadastrarComponent } from './menu-cadastrar/menu-cadastrar.component';
 import { MenuEntrarComponent } from './menu-entrar/menu-entrar.component';
 
 const routes: Routes = [
-    {path:"" , redirectTo:" MenuEntrarComponent",pathMatch:"full"},
+  { path: "", redirectTo: " MenuEntrarComponent", pathMatch: "full" },
 
-    {path:"entrar",component:MenuEntrarComponent},
-    {path:"cadastrar",component:MenuCadastrarComponent}
-  
+  { path: "entrar", component: MenuEntrarComponent },
+  { path: "cadastrar", component: MenuCadastrarComponent },
+  { path: "inicio", component: InicioComponent }
+
+
 ];
 
 @NgModule({
