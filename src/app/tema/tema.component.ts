e9b4ffcd4951362668c5040762eaeab8beb6e85d
ElisCavalcante <elisabeth.rsc@outlook.com>
2022-09-11 16:44:41 -0300

    implementação do get e post de temas ,erro resolvido

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { TemaService } from './../service/tema.service';
+import { Tema } from './../model/Tema';
 import { Router } from '@angular/router';
-
-import { environment } from 'src/environments/environment.prod';
-import { Tema } from '../model/Tema';
-import { TemaService } from '../service/tema.service';
+import { environment } from './../../environments/environment.prod';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tema',
@@ -11,26 +10,39 @@ import { TemaService } from '../service/tema.service';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
-  tema:Tema = new Tema
 
-  constructor(              //injeção de dependêcia
-    private router:Router,
-    private temaService:TemaService
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+  constructor(
+    private router: Router,
+    private temaService: TemaService,
+   
   ) { }
 
-  ngOnInit(){
-    if(environment.token == ""){
-   
-      this.router.navigate(["/entrar"])
+  ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
     }
+
+
+
+    this.findAllTemas()
   }
-  cadastrar(){
-    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
-      this.tema = resp
-      alert('Tema cadastrado com sucesso!')
-     
-      this.tema = new Tema()
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
     })
- }
+  }
+
+  cadastrar(){
+     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+       this.tema = resp
+       alert('Tema cadastrado com sucesso!')
+       this.findAllTemas()
+       this.tema = new Tema()
+     })
+  }
+
 }
-  
